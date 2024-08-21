8ce14b43fcd91d740651f4c6f97cd6b8fbbc7c8e
quentinr <quentin.rotolo@ponant-technologies.com>
2024-08-21 09:12:23 +0200

    building domain model base architecture

--- dataproviders/src/main/kotlin/com/example/todolist/dataproviders/db/jpa/config/DBConfig.kt
+++ dataproviders/src/main/kotlin/com/example/todolist/dataproviders/db/jpa/config/DBConfig.kt
@@ -1,4 +1,11 @@
 package com.example.todolist.dataproviders.db.jpa.config
 
-class DBConfig {
-}
+import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+
+
+@Configuration
+@EntityScan(basePackages = ["com.example.todolist.dataproviders.db.jpa.entities"])
+@EnableJpaRepositories(basePackages = ["com.example.todolist.dataproviders.db.jpa.repositories"])
+class DBConfig
--- dataproviders/src/main/kotlin/com/example/todolist/dataproviders/db/jpa/entities/TodoEntity.kt
+++ dataproviders/src/main/kotlin/com/example/todolist/dataproviders/db/jpa/entities/TodoEntity.kt
@@ -1,2 +1,37 @@
 package com.example.todolist.dataproviders.db.jpa.entities
 
+import com.example.todolist.core.entities.Todo
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import jdk.jfr.Description
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+
+@Entity
+@Table(name = "todos")
+data class TodoEntity(
+    @Id
+    val id: Int,
+    val title: String,
+    val description: Description,
+    val deadline: Long
+)
+
+// Mappers
+fun TodoEntity.toTodo() =
+    Todo(
+        id = this.id,
+        title = this.title,
+        description = this.description,
+        deadline = LocalDateTime.ofInstant(Instant.ofEpochMilli(this.deadline), ZoneId.systemDefault())
+    )
+
+fun Todo.toTodoEntity() =
+    TodoEntity(
+        id = this.id,
+        title = this.title,
+        description = this.description,
+        deadline = this.deadline!!.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli()
+    )
